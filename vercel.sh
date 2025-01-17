91028300c2e0456c69f418995209a0624e0e8484
yutou <1072843139@qq.com>
2025-01-17 15:22:56 +0800

    更新: 优化 Vercel 部署配置

--- vercel.sh
+++ vercel.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# 安装系统依赖
+# 更新包列表
 apt-get update
-apt-get install -y $(cat vercel_requirements.txt)
+
+# 安装系统依赖
+xargs apt-get install -y < vercel_requirements.txt
 
 # 安装 Python 依赖
 pip install -r requirements.txt 
